3229ef8345f3f8dafceeb64691673cb8be0613b2
Pieter Hordijk <info@pieterhordijk.com>
2016-06-11 13:24:20 +0200

    Added base client class
    Signed-off-by: Pieter Hordijk <info@pieterhordijk.com>

---
+++ src/Client.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Gitilicious\GitClient;
+
+class Client
+{
+    private $gitBinary;
+
+    public function __construct(string $gitBinary)
+    {
+        $this->gitBinary = $gitBinary;
+    }
+
+    public function run(string $path, ...$arguments): CommandResult
+    {
+        $descriptorspec = [
+           ['pipe', 'r'],
+           ['pipe', 'w'],
+           ['pipe', 'w'],
+        ];
+
+        $process = proc_open($this->buildCommand($arguments), $descriptorspec, $pipes, $path);
+
+        $stdOut = stream_get_contents($pipes[1]);
+        $stdErr = stream_get_contents($pipes[2]);
+
+        foreach ($pipes as $pipe) {
+            fclose($pipe);
+        }
+
+        return new CommandResult(proc_close($process), $stdOut, $stdErr);
+    }
+
+    private function buildCommand(array $arguments): string
+    {
+        $command = [];
+
+        $command[] = $this->gitBinary;
+
+        foreach ($arguments as $argument) {
+            $command[] = escapeshellarg($argument);
+        }
+
+        return implode(' ', $command);
+    }
+}
