1680a6bfea9bff64e29345a1451e6869f8809862
Pieter Hordijk <info@pieterhordijk.com>
2016-06-11 13:23:49 +0200

    Added command output class
    Signed-off-by: Pieter Hordijk <info@pieterhordijk.com>

---
+++ src/CommandOutput.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Gitilicious\GitClient;
+
+class CommandOutput
+{
+    private $lines = [];
+
+    public function __construct(string $output)
+    {
+        $this->lines = explode(PHP_EOL, trim($output));
+    }
+
+    public function getNumberOfLines(): int
+    {
+        return count($this->lines);
+    }
+
+    public function getLine(int $lineNumber): string
+    {
+        if (!array_key_exists($lineNumber - 1, $this->lines)) {
+            // @todo or should we throw here instead?
+            return '';
+        }
+
+        return $this->lines[$lineNumber - 1];
+    }
+}
