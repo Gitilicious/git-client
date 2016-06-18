0259703b924976d68ba7d562ae9e4e12d2c2afdd
Pieter Hordijk <info@pieterhordijk.com>
2016-06-18 12:04:45 +0200

    Added default readme method
    Signed-off-by: Pieter Hordijk <info@pieterhordijk.com>

--- src/Repository.php
+++ src/Repository.php
@@ -73,6 +73,17 @@ class Repository
         return sprintf('%s/%s/%s', $this->path, $this->owner, $this->name);
     }
 
+    public function addDefaultReadMe()
+    {
+        $repositoryDirectory = $this->path . '/' . $this->owner . '/' . $this->name;
+
+        file_put_contents($repositoryDirectory . '/README.md', "# {$this->name}\n");
+
+        $this->client->run($repositoryDirectory, 'add', 'README.md');
+        $this->client->run($repositoryDirectory, 'commit', '-m', 'Added readme');
+        $this->client->run($repositoryDirectory, 'push');
+    }
+
     public function getBranches(): Branches
     {
         $result = $this->client->run($this->path . '/' . $this->owner . '/' . $this->name, 'branch');
