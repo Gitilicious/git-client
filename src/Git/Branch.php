d37ff05639caf3db6b9fbafca3c9e2f19d3e6ac2
Pieter Hordijk <info@pieterhordijk.com>
2016-06-18 14:01:01 +0200

    Moved git abstractions to dedicated directory
    Signed-off-by: Pieter Hordijk <info@pieterhordijk.com>

--- src/Git/Branch.php
+++ src/Git/Branch.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Gitilicious\GitClient;
+namespace Gitilicious\GitClient\Git;
 
 class Branch
 {
--- src/Git/Repository.php
+++ src/Git/Repository.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Gitilicious\GitClient;
+namespace Gitilicious\GitClient\Git;
 
 use Gitilicious\GitClient\FileSystem\Directory;
 
