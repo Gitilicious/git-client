5a0af711532427557dd43cfce5b1571b73a45f30
Pieter Hordijk <info@pieterhordijk.com>
2016-06-28 21:01:01 +0200

    Removed throws because we are handling it in the client now
    Signed-off-by: Pieter Hordijk <info@pieterhordijk.com>

--- src/Git/Repository.php
+++ src/Git/Repository.php
@@ -15,71 +15,61 @@ class Repository
 
     private $directory;
 
-    private $name;
-
-    public function __construct(Client $client, Directory $directory, string $name)
+    public function __construct(Client $client, Directory $directory)
     {
         $this->client    = $client;
         $this->directory = $directory;
-        $this->name      = $name;
     }
 
-    public static function createBare(Client $client, Directory $directory, string $name): Repository
+    public static function createBare(Client $client, Directory $directory): Repository
     {
-        $repositoryDirectory = $directory->create($directory->getPath() . '/' . $name);
+        $client->run($directory, new PlainArgument('init'), new LongFlag('bare'));
 
-        $result = $client->run($repositoryDirectory, new PlainArgument('init'), new LongFlag('bare'));
-
-        if (!$result->isSuccess()) {
-            throw new Exception($result->getErrorMessage());
-        }
-
-        return new self($client, $directory, $name);
+        return new self($client, $directory);
     }
 
-    public static function create(Client $client, Directory $directory, string $name): Repository
+    public static function create(Client $client, Directory $directory): Repository
     {
-        $repositoryDirectory = $directory->create($directory->getPath() . '/' . $name);
+        $client->run($directory, new PlainArgument('init'));
 
-        $result = $client->run($repositoryDirectory, new PlainArgument('init'));
-
-        if (!$result->isSuccess()) {
-            throw new Exception($result->getErrorMessage());
-        }
-
-        return new self($client, $directory, $name);
+        return new self($client, $directory);
     }
 
-    public function clone(Directory $directory, string $name): Repository
+    public function clone(Directory $directory): Repository
     {
-        $repositoryDirectory = $directory->create($directory->getPath() . '/' . $name);
-
-        $result = $this->client->run(
-            $repositoryDirectory,
+        $this->client->run(
+            $directory,
             new PlainArgument('clone'),
-            new PlainArgument($this->getPath()),
-            new PlainArgument($repositoryDirectory->getPath())
+            new PlainArgument($this->directory->getPath()),
+            new PlainArgument($directory->getPath())
         );
 
-        if (!$result->isSuccess()) {
-            throw new Exception($result->getErrorMessage());
-        }
-
-        return new self($this->client, $directory, $name);
+        return new self($this->client, $directory);
     }
 
     public function getPath(): string
     {
-        return $this->directory->getPath() . '/' . $this->name;
+        return $this->directory->getPath();
+    }
+
+    public function getActiveBranch(): Branch
+    {
+        $branches = $this->getBranches();
+
+        if (!count($branches)) {
+            return new Branch('* master');
+        }
+
+        foreach ($branches as $branch) {
+            if ($branch->isActive()) {
+                return $branch;
+            }
+        }
     }
 
     public function getBranches(): Branches
     {
-        $result = $this->client->run($this->getPath(), 'branch');
-
-        if (!$result->isSuccess()) {
-            throw new Exception('Cannot list the branches.');
-        }
+        $result = $this->client->run($this->directory, new PlainArgument('branch'));
 
         $branches = new Branches($this->client);
 
@@ -88,6 +78,29 @@ class Repository
         }
 
         return $branches;
+    }
+
+    public function createBranch(string $name): Branch
+    {
+        $this->client->run(
+            $this->directory,
+            new PlainArgument('checkout'),
+            new ShortFlag('b'),
+            new PlainArgument($name)
+        );
+
+        return new Branch($name);
+    }
+
+    public function checkout(string $name): Branch
+    {
+        $this->client->run(
+            $this->directory,
+            new PlainArgument('checkout'),
+            new PlainArgument($name)
+        );
+
+        return new Branch('* ' . $name);
     }
 
     public function add(File $file)
